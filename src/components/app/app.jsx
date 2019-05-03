f6a3d707f9a75f3d4b1b965d1fd3428498f7a32d
Settimov7 <prytkov.n@yandex.ru>
2019-05-03 20:13:19 +0400

    Добавляет проверку типов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,16 @@
 import * as React from 'react';
+import * as PropTypes from 'prop-types';
+
 import {Welcome} from "../welcome/welcome";
 
-export const App = () => {
+export const App = (props) => {
+  const {gameTime, errorsCount} = props;
   return (
-    <Welcome />
+    <Welcome time={gameTime} errorsCount={errorsCount} />
   );
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  errorsCount: PropTypes.number.isRequired,
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import * as ReactDOM from 'react-dom';
 
 import {App} from "./components/app/app";
 
+const settings = {
+  gameTime: 5,
+  errorsCount: 3,
+};
+
 const init = () => {
-  ReactDOM.render(<App />, document.querySelector(`.main`));
+  ReactDOM.render(<App {...settings} />, document.querySelector(`.main`));
 };
 
 init();
