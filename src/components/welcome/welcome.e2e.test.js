6c7b60425449ba06878c669b4f37f93572fffee0
Settimov7 <prytkov.n@yandex.ru>
2019-05-04 22:28:03 +0400

    Рефакторинг

--- src/components/welcome/welcome.e2e.test.js
+++ src/components/welcome/welcome.e2e.test.js
@@ -1,10 +1,7 @@
 import * as React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 
 import {Welcome} from "./welcome";
-
-Enzyme.configure({adapter: new Adapter()});
 
 it(`Welcome correctly triggered click event `, () => {
   const props = {
