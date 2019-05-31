97d13d98dad554c105d4c041dd3e0c9697e38d40
Прытков Никита Сергеевич <Nikita_Prytkov@epam.com>
2019-05-31 12:27:00 +0400

    Выносит "магические значения" в переменные

--- src/api.js
+++ src/api.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 import {ActionCreator} from "./reducer/user/user";
 
+const CONFIG = {
+  baseURL: `https://es31-server.appspot.com/guess-melody`,
+  timeout: 5000,
+  withCredentials: true
+};
+
+const HTTP_STATUS = {
+  FORBIDDEN: 403,
+};
+
 export const createAPI = (dispatch) => {
-  const api = axios.create({
-    baseURL: `https://es31-server.appspot.com/guess-melody`,
-    timeout: 1000 * 5,
-    withCredentials: true,
-  });
+  const api = axios.create(CONFIG);
 
   const onSuccess = (response) => response;
   const onFail = (err) => {
-    if (err.response.status === 403) {
+    if (err.response.status === HTTP_STATUS.FORBIDDEN) {
       dispatch(ActionCreator.requireAuthorization(true));
     }
     return err;
